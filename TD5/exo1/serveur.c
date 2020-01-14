8b7748b3732f519758a90d481d0b359332fd56b6
Manon5 <manon.touba05@gmail.com>
2020-01-14 23:55:32 +0100

    Update serveur.c

--- TD5/exo1/serveur.c
+++ TD5/exo1/serveur.c
@@ -1,78 +1,57 @@
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
 #include <sys/types.h>
-#include <unistd.h>
-
-#define PORT 6000
-#define MAX_BUFFER 1000
-
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
 int main(int argc , char const *argv[]) {
-    int fdSocketAttente;
-    int fdSocketCommunication;
-    struct sockaddr_in coordonneesServeur;
-    struct sockaddr_in coordonneesAppelant;
-    char tampon[MAX_BUFFER];
-    int nbRecu;
-    int longueurAdresse;
 
-    fdSocketAttente = socket(PF_INET, SOCK_STREAM, 0);
+   int fdSocketAttente;
+   int fdSocketCommunication;
+   struct sockaddr_in AdServeur;
+   struct sockaddr_in AdClient;
+   int longueurAdresse;
+   int port = 6000; //en vrai jsp
 
-    if (fdSocketAttente < 0) {
-        printf("socket incorrecte\n");
-        exit(EXIT_FAILURE);
+
+   // Créer primitive socket
+  int fdSocketAttente=socket(AF_INET,SOCK_STREAM ,0);
+  if(fdSocketAttente== -1) {
+    printf("Erreur de création de socket\n");
+    exit(0);
+  }
+
+
+  // Création structure d'adresse
+  longueurAdresse = sizeof(struct sockaddr_in);
+  memset(&AdServeur , 0x00, longueurAdresse);
+
+  AdServeur.sin_family = AF_INET;
+  AdServeur.sin_addr.s_addr = htonl(INADDR_ANY);
+  // toutes les interfaces locales disponibles
+  AdServeur.sin_port = htons(6000);
+  //permet d’attendre la connexion sur le port 6000
+
+  // Bind
+  if(bind(fdSocketAttente,(struct sockaddr*)&AdServeur ,longueurAdresse) == -1) {
+    printf("Erreur de  bind \n");
+     exit(0);
+  }
+
+  // le serveur se met en attente de connexion
+  if(listen(fdSocketAttente ,5)== -1) {
+    printf("Erreur de listen \n");
+    exit(0);
+  }
+
+    printf("Le serveur est en attente d'une connexion\n");
+
+    int longueurAdresse=sizeof(AdClient);
+    int fdSocketCommunication=accept(fdSocketAttente,(struct sockaddr*)&AdClient ,&longueurAdresse);
+    if(fdSocketCommunication == -1) {
+        printf("Erreur d’accept \n");
+        exit(0);
     }
 
-    // On prépare l’adresse d’attachement locale
-    longueurAdresse = sizeof(struct sockaddr_in);
-    memset(&coordonneesServeur, 0x00, longueurAdresse);
-
-    coordonneesServeur.sin_family = PF_INET;
-    // toutes les interfaces locales disponibles
-    coordonneesServeur.sin_addr.s_addr = htonl(INADDR_ANY);
-    // toutes les interfaces locales disponibles
-    coordonneesServeur.sin_port = htons(PORT);
-
-    if (bind(fdSocketAttente, (struct sockaddr *) &coordonneesServeur, sizeof(coordonneesServeur)) == -1) {
-        printf("erreur de bind\n");
-        exit(EXIT_FAILURE);
-    }
-
-    if (listen(fdSocketAttente, 5) == -1) {
-        printf("erreur de listen\n");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("En attente de connexion...\n");
-
-    socklen_t tailleCoord = sizeof(coordonneesAppelant);
-
-    if ((fdSocketCommunication = accept(fdSocketAttente, (struct sockaddr *) &coordonneesAppelant,
-                                        &tailleCoord)) == -1) {
-        printf("erreur de accept\n");
-        exit(-1);
-    }
-
-    printf("Client connecté\n");
-
-    // on attend le message du client
-    // la fonction recv est bloquante
-    nbRecu = recv(fdSocketCommunication, tampon, MAX_BUFFER, 0);
-
-    if (nbRecu > 0) {
-        tampon[nbRecu] = 0;
-        printf("Recu : %s\n", tampon);
-    }
-
-    strcpy(tampon, "Message renvoyé par le serveur vers le client !");
-    // on envoie le message au client
-    send(fdSocketCommunication, tampon, strlen(tampon), 0);
-
-    close(fdSocketCommunication);
-    close(fdSocketAttente);
-
-    return EXIT_SUCCESS;
 }
+
